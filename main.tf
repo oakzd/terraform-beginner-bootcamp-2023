33d52c668bd23bbc08e1a126ac30b36a08f983ca
oakzd Abu <oakzaid@gmail.com>
2023-10-02 16:31:19 +0000

    # 11 right before we get the s3 bucket

--- main.tf
+++ main.tf
@@ -4,17 +4,35 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+      aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+
   }
+}
+
+# Providers below
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
   # Configuration options
 }
+# Providers above
 
+# Resources below
 resource "random_string" "bucket_name" {
   length   = 16
   special  = false
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+}
+
+#Resources above
 
 output "random_bucket_name" {
   value = random_string.bucket_name.result
