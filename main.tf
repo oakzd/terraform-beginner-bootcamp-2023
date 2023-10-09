19640714ed756d9758bf7dc10f0d3079a7873dd7
oakzd Abu <oakzaid@gmail.com>
2023-10-09 16:05:36 +0000

    # 11 install terraform provider ,create s3 bucket

--- main.tf
+++ main.tf
@@ -22,14 +22,21 @@ provider "random" {
 }
 # Providers above
 
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 # Resources below
 resource "random_string" "bucket_name" {
-  length   = 16
+  lower = true
+  upper = false
+  length   = 32
   special  = false
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
+
 }
 
 #Resources above
