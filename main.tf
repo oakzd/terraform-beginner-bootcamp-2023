d20c8d43aa8223516f9ed43130a734173b3d4638
oakzd Abu <oakzaid@gmail.com>
2023-11-26 04:38:05 +0000

    #44 finish implementation of terraform provider

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -37,14 +37,14 @@ provider "terratowns" {
 #  content_version = var.content_version
 #}
 
-resource "terrahouse_home" "home"{
-  name = "People of Palestine"
+resource "terratowns_home" "home"{
+  name = "People of Palestine!"
   description = <<DESCRIPTION
   The Palestinan people are a people of honor and endurance. They have been under occupation for over 75 years!
   They continue to resist their occupier and push for a FREE PALESTINE!
   DESCRIPTION
   #domain_name =module.terrahouse_aws.cloudfront_url
-  domain_name = "234sdf.cloundfront.net"
+  domain_name = "234sdf.cloudfront.net"
   town = "missingo"
   content_version = 1
 }
